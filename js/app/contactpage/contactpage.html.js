aae9b133a8d0f4a60798418afbc8a8ea9583d88b
HDwares <deepeshsom@gmail.com>
2021-04-27 12:59:21 +0530

    mail-id recolor

--- js/app/contactpage/contactpage.html.js
+++ js/app/contactpage/contactpage.html.js
@@ -1,3 +1,3 @@
 export let contactpage=`        <div class="frame" id="contact">
-    <span>Contact today at <a href="mailto:horizontechs100@gmail.com">horizontechs100@gmail.com</a> for your website creation.<span>
+    <span>Contact today at <a class="mail-id" href="mailto:horizontechs100@gmail.com">horizontechs100@gmail.com</a> for your website creation.<span>
 </div>`
