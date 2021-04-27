b38d993eca0de4159dac910d1e684851e1ca1c76
HDwares <deepeshsom@gmail.com>
2021-04-27 12:53:14 +0530

    brand-logo sizing

--- js/app/homepage/brand-logo/brand-logo.component.js
+++ js/app/homepage/brand-logo/brand-logo.component.js
@@ -5,12 +5,9 @@ export function flyout_brand_logo_corner(){
     return new Promise((resolve)=>{
         let brandLogo= document.getElementById("brand-logo");
         setTimeout(()=>{
-            brandLogo.style.width="10%";
+            brandLogo.style.width="calc(5rem + 2vw)";
             brandLogo.style.margin="0";
             resolve("brand logo moved");
-            // bring_nav_bar_to_display();
-            // document.getElementById("welcome-circle").style.display="block";
-            // arorizeCircle();
         }, 1200)
     })
 }
