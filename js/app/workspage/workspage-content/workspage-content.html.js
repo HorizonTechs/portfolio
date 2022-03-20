d78dfc54ec2810aa264f70138e72f6c4f1c68a3d
HDWares <deepeshsom@gmail.com>
2022-03-20 23:04:41 +0530

    new work added

--- js/app/workspage/workspage-content/workspage-content.html.js
+++ js/app/workspage/workspage-content/workspage-content.html.js
@@ -46,4 +46,27 @@ export let workspageContent = `
                 </div>
             </div>
         </div>
+        <div class="feature-tile">
+            <h2><span class="redTxt">Society </span> Management System
+                <a href="https://hd-society-management-system.herokuapp.com/" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas"
+                        data-icon="external-link-alt" class="svg-inline--fa fa-external-link-alt fa-w-16" role="img"
+                        viewBox="0 0 512 512">
+                        <path fill="currentColor"
+                            d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z" />
+                    </svg>
+                </a>
+            </h2>
+            <div class="feature-detail">
+                <div class="thumbnail-container">
+                    <img src="img/works3.png" alt="works3" loading="lazy">
+                </div>
+                <div>
+                    <p>Dynamic website with user and society collection management facility</p>
+                    <p>SPA with routing</p>
+                    <p>Lots of features including add update and delete records
+                     like user, societies, collections, incomes, expenses, login-logout</p>
+                </div>
+            </div>
+        </div>
     </div>`
